17852163203e843adb811f4bd63ef8de078476b3
Ricardo Ferreira Ribeiro <ricardo.ferib@gmail.com>
2019-06-27 19:06:47 -0300

    Initial commit

---
+++ src/main/java/com/springcouse/SpringCouseApplication.java
@@ -0,0 +1,13 @@
+package com.springcouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCouseApplication.class, args);
+	}
+
+}
