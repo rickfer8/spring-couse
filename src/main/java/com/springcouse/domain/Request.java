33cd3c56d37969b09733c435eedd0907af6a567e
Ricardo Ferreira Ribeiro <ricardo.ferib@gmail.com>
2019-06-28 11:26:14 -0300

    Mapped class

--- src/main/java/com/springcouse/domain/Request.java
+++ src/main/java/com/springcouse/domain/Request.java
@@ -1,8 +1,22 @@
 package com.springcouse.domain;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 import com.springcouse.domain.enuns.RequestState;
 
@@ -14,14 +28,36 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter @Setter
-public class Request {
+@Entity(name = "request")
+public class Request implements Serializable {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(length = 75, nullable = false)
 	private String subject;
+	@Column(columnDefinition = "text")
 	private String description;
+	
+	@Column(name = "criation_date", nullable = false)
+	@Temporal(TemporalType.DATE)
 	private Date criationDate;
+	
+	@Column(length = 12, nullable = false)
+	@Enumerated(EnumType.STRING)
 	private RequestState state;
+	
+	@ManyToOne
+	@JoinColumn(name = "user_id", nullable = false)
 	private User user;
+	
+	@OneToMany(mappedBy = "request")
 	private List<RequestStage> stages = new ArrayList<RequestStage>();
 	
 }
--- src/main/java/com/springcouse/domain/User.java
+++ src/main/java/com/springcouse/domain/User.java
@@ -1,7 +1,18 @@
 package com.springcouse.domain;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 import com.springcouse.domain.enuns.Role;
 
@@ -13,14 +24,35 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter @Setter
-public class User {
+@Entity(name = "user")
+public class User implements Serializable {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(length = 75, nullable = false)
 	private String name;
+	
+	@Column(length = 75, nullable = false, unique = true)
 	private String email;
+	
+	@Column(length = 100, nullable = false)
 	private String password;
+	
+	@Column(length = 20, nullable = false)
+	@Enumerated(EnumType.STRING)
 	private Role role;
+	
+	@OneToMany(mappedBy = "user")
 	private List<Request> requests = new ArrayList<Request>();
+	
+	@OneToMany(mappedBy = "user")
 	private List<RequestStage> stages = new ArrayList<RequestStage>();
 
 }
