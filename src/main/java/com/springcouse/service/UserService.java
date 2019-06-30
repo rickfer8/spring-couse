e9c640abfdd7352498d35993a09881022b033cfc
Ricardo Ferreira Ribeiro <ricardo.ferib@gmail.com>
2019-06-30 19:14:58 -0300

    Created the Class User Service

---
+++ src/main/java/com/springcouse/service/UserService.java
@@ -0,0 +1,47 @@
+package com.springcouse.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.springcouse.domain.User;
+import com.springcouse.repository.UserRepository;
+import com.springcouse.service.util.HashUtil;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	public User save(User user) {
+		String hash = HashUtil.getSecureHash(user.getPassword());
+		user.setPassword(hash);
+		User createdUser = userRepository.save(user);
+		return createdUser;
+	}
+	
+	public User update(User user) {
+		User updateUser = userRepository.save(user);
+		return updateUser;
+	}
+	
+	public User getById(Long id) {
+		Optional<User> result = userRepository.findById(id);
+		return result.get();
+	}
+	
+	public List<User> listAll(){
+		List<User> users = userRepository.findAll();
+		return users;
+	}
+	
+	public User login(String email, String password) {
+		password = HashUtil.getSecureHash(password);
+		Optional<User> result = userRepository.login(email, password);
+		return result.get();
+	}
+
+}
---
+++ src/test/java/com/springcouse/service/util/HashUtilTests.java
@@ -0,0 +1,20 @@
+package com.springcouse.service.util;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class HashUtilTests {
+	
+	@Test
+	public void getSecureHashText() {
+		String hash = HashUtil.getSecureHash("123");
+		System.out.println(hash);
+		
+		assertThat(hash.length()).isEqualTo(64);
+	}
+
+}
