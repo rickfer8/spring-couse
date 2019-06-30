41f59cd31235d2fafef854dbb5fc9b32ef33bed4
Ricardo Ferreira Ribeiro <ricardo.ferib@gmail.com>
2019-06-30 19:23:16 -0300

    Created Request Service

---
+++ src/main/java/com/springcouse/service/RequestService.java
@@ -0,0 +1,50 @@
+package com.springcouse.service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.springcouse.domain.Request;
+import com.springcouse.domain.User;
+import com.springcouse.domain.enuns.RequestState;
+import com.springcouse.repository.RequestRepository;
+
+@Service
+public class RequestService {
+	
+	@Autowired
+	private RequestRepository requestRepository;
+	
+	public Request save(Request request) {
+
+		Request createdRequest = requestRepository.save(request);
+		return createdRequest;
+	}
+	
+	public Request update(Request request) {
+		request.setCriationDate(new Date());
+		request.setState(RequestState.OPEN);
+		
+		Request updateRequest = requestRepository.save(request);
+		return updateRequest;
+	}
+	
+	public Request getById(Long id) {
+		Optional<Request> result = requestRepository.findById(id);
+		return result.get();
+	}
+	
+	public List<Request> listAll(){
+		List<Request> requests = requestRepository.findAll();
+		return requests;
+	}
+	
+	public List<Request> listAllByOwnerId(Long ownerId){
+		List<Request> requests = requestRepository.findAllByOwnerId(ownerId);
+		return requests;
+	}
+
+}
