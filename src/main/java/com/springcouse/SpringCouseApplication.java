a3ed645ea0a5b413249ef979b7d92ab9daedf259
Ricardo Ferreira Ribeiro <ricardo.ferib@gmail.com>
2019-07-02 21:11:59 -0300

    Configuration the Class Spring Course Application

--- src/main/java/com/springcouse/SpringCouseApplication.java
+++ src/main/java/com/springcouse/SpringCouseApplication.java
@@ -2,9 +2,16 @@ package com.springcouse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringCouseApplication {
+public class SpringCouseApplication extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringCouseApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCouseApplication.class, args);
